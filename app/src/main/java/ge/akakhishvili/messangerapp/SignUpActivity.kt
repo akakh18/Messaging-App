11f8d638b929f49934b209248f6876ce3627882f
Kobbit <kpaik18@freeuni.edu.ge>
2022-07-16 15:23:32 +0400

    Add SignUpActivity layout

---
+++ app/src/main/java/ge/akakhishvili/messangerapp/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package ge.akakhishvili.messangerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+    }
+}
