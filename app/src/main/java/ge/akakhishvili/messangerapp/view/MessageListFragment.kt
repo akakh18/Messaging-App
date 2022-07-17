c36c59bc3e580016e9a3a73c9a5895893512848e
Kobbit <kpaik18@freeuni.edu.ge>
2022-07-17 14:37:10 +0400

    message list layout done

---
+++ app/src/main/java/ge/akakhishvili/messangerapp/view/MessageListFragment.kt
@@ -0,0 +1,24 @@
+package ge.akakhishvili.messangerapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ge.akakhishvili.messangerapp.R
+
+class MessageListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_message_list, container, false)
+    }
+
+}
