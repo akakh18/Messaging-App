a943b6307442923ad9bcc69648540d255670b16b
Koba Paikidze <koba.paikidze@ol.ge>
2022-07-19 15:38:52 +0400

    layout of chat page added

---
+++ app/src/main/java/ge/akakhishvili/messangerapp/view/ChatActivity.kt
@@ -0,0 +1,12 @@
+package ge.akakhishvili.messangerapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ge.akakhishvili.messangerapp.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
