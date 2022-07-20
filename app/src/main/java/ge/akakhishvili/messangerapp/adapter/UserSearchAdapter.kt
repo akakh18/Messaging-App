8802e9b564af3882701a518b62e45a1a253cdbd9
Koba Paikidze <koba.paikidze@ol.ge>
2022-07-20 12:01:31 +0400

    chat message layouts added

--- app/src/main/java/ge/akakhishvili/messangerapp/adapter/UserSearchAdapter.kt
+++ app/src/main/java/ge/akakhishvili/messangerapp/adapter/UserSearchAdapter.kt
@@ -1,11 +1,14 @@
 package ge.akakhishvili.messangerapp.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import ge.akakhishvili.messangerapp.R
+import ge.akakhishvili.messangerapp.view.ChatActivity
 import ge.akakhishvili.messangerapp.view.UserProfile
 
 class UserSearchAdapter(var profiles: List<UserProfile>): RecyclerView.Adapter<UserSearchItemViewHolder>() {
@@ -28,7 +31,6 @@ class UserSearchItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView
     fun bind(profile: UserProfile) {
         itemView.findViewById<TextView>(R.id.user_search_item_nickname).setText(profile.username)
         itemView.findViewById<TextView>(R.id.user_search_item_career).setText(profile.career)
-
     }
 
 }
