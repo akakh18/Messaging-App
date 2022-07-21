a61ef3b8f1e90664bf7d12ca4bc49e4557ced4ff
Koba Paikidze <koba.paikidze@ol.ge>
2022-07-21 17:21:11 +0400

    message fetching started

--- app/src/main/java/ge/akakhishvili/messangerapp/service/GeneralChatService.kt
+++ app/src/main/java/ge/akakhishvili/messangerapp/service/GeneralChatService.kt
@@ -2,10 +2,16 @@ package ge.akakhishvili.messangerapp.service
 
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.HashMap
 
 class GeneralChatService {
     fun fetchChatsForUser(currentUserId: String) {
         var messagesRef = Firebase.database.getReference("messages")
-
+        messagesRef.get().addOnSuccessListener {
+            var data = it
+            var messages = it.value
+            println(messages)
+        }
     }
 }
