17adc8bafbb0c110cc11623b752395977a27e11f
Kobbit <kpaik18@freeuni.edu.ge>
2022-07-22 19:38:03 +0400

    user chats fetched successfully

--- app/src/main/java/ge/akakhishvili/messangerapp/service/GeneralChatService.kt
+++ app/src/main/java/ge/akakhishvili/messangerapp/service/GeneralChatService.kt
@@ -1,17 +1,82 @@
 package ge.akakhishvili.messangerapp.service
 
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.database.ktx.FirebaseDatabaseKtxRegistrar
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
 class GeneralChatService {
     fun fetchChatsForUser(currentUserId: String) {
         var messagesRef = Firebase.database.getReference("messages")
         messagesRef.get().addOnSuccessListener {
-            var data = it
-            var messages = it.value
-            println(messages)
+            var messages = it.value as HashMap<String, Object>
+            var userChats = getUserChats(currentUserId, messages)
+            var userChatsWithNicknames = arrayListOf<UserKeyWithLastMessageAndNickname>()
+            var profilesRef = Firebase.database.getReference("profiles")
+            var profiles = profilesRef.get().addOnSuccessListener {
+                var profilesData = it.value as HashMap<String, HashMap<String, String>>
+                for(user in userChats){
+                    var nextUserData = profilesData[user.secondUserKey]
+                    userChatsWithNicknames.add(
+                        UserKeyWithLastMessageAndNickname(
+                            user.secondUserKey,
+                            nextUserData?.get("username")!!,
+                            user.lastMessage
+                        )
+                    )
+                }
+                println(userChatsWithNicknames)
+            }
         }
     }
+
+    private fun getUserChats(currentUserId: String, messages: HashMap<String, Object>): ArrayList<UserKeyWithLastMessage>{
+        var userChats = arrayListOf<UserKeyWithLastMessage>()
+        for((k, v) in messages){
+            var tokens = k.split("_____")
+            var firstKey = tokens[0]
+            var secondKey = tokens[1]
+            var userKeyWithLastMessage: UserKeyWithLastMessage
+            if(firstKey.equals(currentUserId)){
+                userKeyWithLastMessage = makeUserKeyWithLastMessage(firstKey, v)
+                userChats.add(userKeyWithLastMessage)
+            }else if(secondKey.equals(currentUserId)){
+                userKeyWithLastMessage = makeUserKeyWithLastMessage(firstKey, v)
+                userChats.add(userKeyWithLastMessage)
+            }
+        }
+        return userChats
+    }
+
+    private fun makeUserKeyWithLastMessage(key: String, v: Object): UserKeyWithLastMessage {
+        var messageMap = v as HashMap<String, HashMap<String, String>>
+        var messagesList = messageMap.values.toList()
+
+        var mappedMessages = arrayListOf<Message>()
+        for(m in messagesList) {
+            var message = Message(m["message"]!!,
+                m["messageTime"] as Long,
+                m["receiverUserId"],
+                m["senderUserId"])
+            mappedMessages.add(message)
+        }
+
+        mappedMessages.sortByDescending{
+            it.messageTime
+        }
+        if(mappedMessages.size > 0){
+            return UserKeyWithLastMessage(key, mappedMessages[0].message!!)
+        }
+        return UserKeyWithLastMessage(key, "")
+    }
 }
+
+data class UserKeyWithLastMessageAndNickname(val secondUserKey: String,
+                                             val secondUserNickname: String,
+                                             val lastMessage: String)
+
+data class UserKeyWithLastMessage(val secondUserKey: String,
+                                  val lastMessage: String)
