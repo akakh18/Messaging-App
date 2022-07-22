791e7269f16097f0104ce1648ca82e0394253cab
akakh18 <akakh18@freeuni.edu.ge>
2022-07-23 01:44:26 +0400

    Sort messages on main page

--- app/src/main/java/ge/akakhishvili/messangerapp/service/GeneralChatService.kt
+++ app/src/main/java/ge/akakhishvili/messangerapp/service/GeneralChatService.kt
@@ -6,21 +6,19 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import ge.akakhishvili.messangerapp.view.`interface`.IMessageListView
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 class GeneralChatService(val view: IMessageListView) {
     fun fetchChatsForUser(currentUserId: String, searchKeyword: String?) {
-        var messagesRef = Firebase.database.getReference("messages")
+        val messagesRef = Firebase.database.getReference("messages")
         messagesRef.get().addOnSuccessListener {
-            var messages = it.value as HashMap<String, Object>
-            var userChats = getUserChats(currentUserId, messages)
-            var userChatsWithNicknames = arrayListOf<UserKeyWithLastMessageAndNickname>()
-            var profilesRef = Firebase.database.getReference("profiles")
+            val messages = it.value as HashMap<String, Object>
+            val userChats = getUserChats(currentUserId, messages)
+            val userChatsWithNicknames = arrayListOf<UserKeyWithLastMessageAndNickname>()
+            val profilesRef = Firebase.database.getReference("profiles")
             profilesRef.get().addOnSuccessListener {
-                var profilesData = it.value as HashMap<String, HashMap<String, String>>
-                for(user in userChats){
-                    var nextUserData = profilesData[user.secondUserKey]
+                val profilesData = it.value as HashMap<String, HashMap<String, String>>
+                for (user in userChats) {
+                    val nextUserData = profilesData[user.secondUserKey]
                     userChatsWithNicknames.add(
                         UserKeyWithLastMessageAndNickname(
                             user.secondUserKey,
@@ -32,27 +30,33 @@ class GeneralChatService(val view: IMessageListView) {
                     )
                 }
                 var searchedList = userChatsWithNicknames
-                if(searchKeyword != null){
+                if (searchKeyword != null) {
                     searchedList = userChatsWithNicknames.filter { u ->
-                        u.secondUserNickname!!.lowercase().contains(searchKeyword!!.lowercase()) } as ArrayList<UserKeyWithLastMessageAndNickname>
+                        u.secondUserNickname.lowercase().contains(searchKeyword.lowercase())
+                    } as ArrayList<UserKeyWithLastMessageAndNickname>
                 }
+
+                searchedList.sortBy { -1 * it.messageTime }
                 view.updateChats(searchedList)
 
             }
         }
     }
 
-    private fun getUserChats(currentUserId: String, messages: HashMap<String, Object>): ArrayList<UserKeyWithLastMessage>{
-        var userChats = arrayListOf<UserKeyWithLastMessage>()
-        for((k, v) in messages){
-            var tokens = k.split("_____")
-            var firstKey = tokens[0]
-            var secondKey = tokens[1]
+    private fun getUserChats(
+        currentUserId: String,
+        messages: HashMap<String, Object>
+    ): ArrayList<UserKeyWithLastMessage> {
+        val userChats = arrayListOf<UserKeyWithLastMessage>()
+        for ((k, v) in messages) {
+            val tokens = k.split("_____")
+            val firstKey = tokens[0]
+            val secondKey = tokens[1]
             var userKeyWithLastMessage: UserKeyWithLastMessage
-            if(firstKey.equals(currentUserId)){
+            if (firstKey.equals(currentUserId)) {
                 userKeyWithLastMessage = makeUserKeyWithLastMessage(secondKey, v)
                 userChats.add(userKeyWithLastMessage)
-            }else if(secondKey.equals(currentUserId)){
+            } else if (secondKey.equals(currentUserId)) {
                 userKeyWithLastMessage = makeUserKeyWithLastMessage(firstKey, v)
                 userChats.add(userKeyWithLastMessage)
             }
@@ -61,29 +65,35 @@ class GeneralChatService(val view: IMessageListView) {
     }
 
     private fun makeUserKeyWithLastMessage(key: String, v: Object): UserKeyWithLastMessage {
-        var messageMap = v as HashMap<String, HashMap<String, String>>
-        var messagesList = messageMap.values.toList()
+        val messageMap = v as HashMap<String, HashMap<String, String>>
+        val messagesList = messageMap.values.toList()
 
-        var mappedMessages = arrayListOf<Message>()
-        for(m in messagesList) {
-            var message = Message(m["message"]!!,
+        val mappedMessages = arrayListOf<Message>()
+        for (m in messagesList) {
+            val message = Message(
+                m["message"]!!,
                 m["messageTime"] as Long,
                 m["receiverUserId"],
-                m["senderUserId"])
+                m["senderUserId"]
+            )
             mappedMessages.add(message)
         }
 
-        mappedMessages.sortByDescending{
+        mappedMessages.sortByDescending {
             it.messageTime
         }
-        if(mappedMessages.size > 0){
-            return UserKeyWithLastMessage(key, mappedMessages[0].message!!, mappedMessages[0].messageTime!!)
+        if (mappedMessages.size > 0) {
+            return UserKeyWithLastMessage(
+                key,
+                mappedMessages[0].message!!,
+                mappedMessages[0].messageTime!!
+            )
         }
         return UserKeyWithLastMessage(key, "", 0L)
     }
 
     fun addChatsListener(currentUserId: String) {
-        var messagesRef = Firebase.database.getReference("messages")
+        val messagesRef = Firebase.database.getReference("messages")
         messagesRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 fetchChatsForUser(currentUserId, null)
@@ -100,12 +110,16 @@ class GeneralChatService(val view: IMessageListView) {
     }
 }
 
-data class UserKeyWithLastMessageAndNickname(val secondUserKey: String,
-                                             val secondUserNickname: String,
-                                             val lastMessage: String,
-                                             val messageTime: Long,
-                                             val secondUserCareer: String)
+data class UserKeyWithLastMessageAndNickname(
+    val secondUserKey: String,
+    val secondUserNickname: String,
+    val lastMessage: String,
+    val messageTime: Long,
+    val secondUserCareer: String
+)
 
-data class UserKeyWithLastMessage(val secondUserKey: String,
-                                  val lastMessage: String,
-                                  val messageTime: Long, )
+data class UserKeyWithLastMessage(
+    val secondUserKey: String,
+    val lastMessage: String,
+    val messageTime: Long,
+)
